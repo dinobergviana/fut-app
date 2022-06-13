42a5471ddfb5973457fae2695760908dc5581ec4
dinobergviana <dinobergueviana@gmail.com>
2022-06-13 00:11:56 -0300

    setting up project

---
+++ src/views/Matches.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Partidas</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+// import axios from 'axios';
+
+@Component({
+  components: {},
+})
+export default class Home extends Vue {
+  private apiKey = 'live_38e06ca1acea3770a689a24d7eac77'
+
+  // private async created() {
+  //   const response = await axios.get('https://api.api-futebol.com.br/v1/campeonatos', {
+  //     headers: {
+  //       Authorization: `Bearer ${this.apiKey}`,
+  //     },
+  //   });
+  //   console.log(response);
+  // }
+}
+</script>
---
+++ src/views/Placing.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="placing">
+    <h1>Tabela</h1>
+  </div>
+</template>
