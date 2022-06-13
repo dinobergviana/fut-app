5d511efbb16c25e4438853628ccde2524c09086b
dinobergviana <dinobergueviana@gmail.com>
2022-06-13 02:02:42 -0300

    refactoring App componente styling

--- src/App.vue
+++ src/App.vue
@@ -32,42 +32,45 @@
   background-color: #10502f;
   color: #fff;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0);
-}
 
-.nav__title {
-  padding: 10px 20px;
-  font-size: 16px;
-  line-height: 20px;
-  font-weight: bold;
-}
+  &__title {
+    padding: 10px 20px;
 
-.nav__options {
-  display: flex;
-  align-items: end;
-  justify-content: center;
-  gap: 20px;
-
-  height: 100px;
-
-  a {
-    text-transform: uppercase;
-    text-decoration: none;
-    color: #fff;
-    padding: 15px 20px;
-    font-size: 12px;
+    font-size: 16px;
+    line-height: 20px;
     font-weight: bold;
+  }
 
-    opacity: 0.7;
+  &__options {
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    gap: 20px;
 
-    &:hover {
-      opacity: 1;
-      background: rgba(43, 168, 91, 0.2);
-    }
+    height: 100px;
 
-    &.router-link-exact-active {
-      opacity: 1;
-      border-bottom: 3px solid #fff;
-      background: rgba(43, 168, 91, 0.2);
+    a {
+      text-transform: uppercase;
+      text-decoration: none;
+      color: #fff;
+
+      padding: 15px 20px;
+
+      font-size: 12px;
+      font-weight: bold;
+
+      opacity: 0.7;
+
+      &:hover {
+        opacity: 1;
+        background: rgba(43, 168, 91, 0.2);
+      }
+
+      &.router-link-exact-active {
+        opacity: 1;
+        border-bottom: 3px solid #fff;
+        background: rgba(43, 168, 91, 0.2);
+      }
     }
   }
 }
